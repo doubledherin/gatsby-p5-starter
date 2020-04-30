0a606389ff7fcf5326ccdab8d070e4f6c4ce0406
Wendy Dherin <wendydherin@gmail.com>
2020-04-30 18:46:23 +0200

    Finish gallery layout.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from '@emotion/styled'
 import { Global, css } from '@emotion/core'
 
 import Header from './header'
@@ -10,12 +9,25 @@ const Layout = ({ children }) => {
     <div>
       <Global
       styles={css`
-        body {
+        * {
+          box-sizing: border-box;
+        }
+        html, body {
           background: black;
           color: white;
         }
+        html, body, div, img {
+          margin: 0;
+          padding: 0;
+          border: 0;
+          font-size: 100%;
+          font: inherit;
+          vertical-align: baseline;
+        }
+        body {
+            line-height: 1;
+        }
         header {
-          background: pink;
           top: 0;
           left: 0;
           position: absolute;
