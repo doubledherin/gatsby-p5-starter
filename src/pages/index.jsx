ca885587f6491f0dd658aef09c81a39a707e2ffb
Wendy Dherin <wendydherin@gmail.com>
2020-05-02 14:29:44 +0200

    Go back to using slinky sketch for landing page.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Sketch from '../components/sketch'
-import marredPotential from '../scripts/sketches/marredPotential'
+import slinky from '../scripts/sketches/slinky'
 
 const IndexPage = () => {
   return (
     <div>
       <Layout>
         <SEO />
-        <Sketch sketch={marredPotential}/>
+        <Sketch sketch={slinky}/>
       </Layout>
     </div>
   )
