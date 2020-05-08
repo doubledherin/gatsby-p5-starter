8207b5a3954f8e567c9453a1c6f16951d4eaa586
Wendy Dherin <wendydherin@gmail.com>
2020-05-08 13:01:52 +0200

    Put main.scss in styles directory and update references.

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { Link } from "gatsby"
 
-import "../../main.scss"
+import "../../styles/main.scss"
 
 const Header = () => {
 
