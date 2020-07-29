6c0d9b059a8ac220e4b471581ec587cec88e138a
Wendy Dherin <wendydherin@gmail.com>
2020-07-29 09:49:22 +0100

    Use .eslintignore instead of ignorePatterns setting in .eslintrc.js.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  "ignorePatterns": ["p5.min.js", "eslintrc.js"],
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
