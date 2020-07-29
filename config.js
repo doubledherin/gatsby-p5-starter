450ac9a614c1030f15b38ed8a49679836a2d47ce
Wendy Dherin <wendydherin@gmail.com>
2020-07-29 15:49:43 +0100

    Set pathPrefix to empty string

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteShortDescription: '',
   siteKeywords: 'comma, separated, list', // Replace this with your SEO keywords
   siteUrl: 'https://your-username.github.io',
-  pathPrefix: '/my-gatsby-p5-site',
+  pathPrefix: '',
   siteLanguage: 'en',
   get copyright() {
     return `Copyright \u00A9 ${new Date().getFullYear()} ${this.siteAuthor}`
