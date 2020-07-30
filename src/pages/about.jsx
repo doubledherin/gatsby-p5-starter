19a06c24dae25a25655fb78e91f3135bd64830bf
Wendy Dherin <wendydherin@gmail.com>
2020-07-30 16:01:01 +0100

    Update the About and Contact sections.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from 'gatsby'
+
 import PropTypes from 'prop-types'
 import Img from "gatsby-image/withIEPolyfill"
 
@@ -11,12 +12,19 @@ const About = ({ data }) => {
   return (
     <Layout>
       <div className="about-content-container">
+        <h1>About the Gallery</h1>
+        <p>Most works are interactive via click and drag. Read the p5.js <a href="https://github.com/doubledherin/gatsby-p5-starter/tree/master/src/scripts/sketches" target="_blank" rel="noreferrer">scripts</a> to know all of the details on how to interact.</p>
+        <p>Most works are interactive via click and drag. Read the p5.js <a href="https://github.com/doubledherin/gatsby-p5-starter/tree/master/src/scripts/sketches" target="_blank" rel="noreferrer" >scripts</a> to know all of the details on how to interact.</p>
+        <p>To download a snapshot of a work, hit the &lsquo;s&rsquo; key.</p>
+        <p>Licensing note: Except for &ldquo;Perlin Noise Flower,&rdquo; which is mine, the works displayed in the gallery are taken from a book called <a href="https://benedikt-gross.de/projects/generative-design-visualize-program-create-with-javascript-in-p5-js" target="_blank" rel="noreferrer">Generative Design</a> and have been slightly modified, mostly for technical reasons. This is fair use according to the works&rsquo; Apache licenses, which are included in the works&rsquo; scripts in the codebase.</p>
+        <h1>About This Site</h1>
+        <p>This site was created using <a href="https://github.com/doubledherin/gatsby-p5-starter" target="_blank" rel="noreferrer">this Gatsby starter</a>, which I built so that it&rsquo;s easy to create and deploy a snazzy responsive gallery website that features p5.js works. You can duplicate this site for your own p5.js works! To learn how check out the <a href="https://github.com/doubledherin/gatsby-p5-starter/blob/master/README.md" target="_blank" rel="noreferrer">readme of my starter</a>.</p>
         <h1>About Me</h1>
         { data && nodes && <Img className="round" fluid={ nodes[0].childImageSharp.fluid } /> }
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste ad numquam rerum distinctio nesciunt fuga in? Numquam error quia aspernatur est totam tenetur laboriosam accusamus. Odio non consequatur dicta!Voluptatum error voluptatibus amet esse qui autem, iure eligendi alias laboriosam corporis magni animi cumque soluta quis! Provident cum quia voluptate ab. Cum fugiat eaque possimus consectetur maiores pariatur earum.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste ad numquam rerum distinctio nesciunt fuga in? Numquam error quia aspernatur est totam tenetur laboriosam accusamus. Odio non consequatur dicta!Voluptatum error voluptatibus amet esse qui autem, iure eligendi alias laboriosam corporis magni animi cumque soluta quis! Provident cum quia voluptate ab. Cum fugiat eaque possimus consectetur maiores pariatur earum.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste ad numquam rerum distinctio nesciunt fuga in? Numquam error quia aspernatur est totam tenetur laboriosam accusamus. Odio non consequatur dicta!Voluptatum error voluptatibus amet esse qui autem, iure eligendi alias laboriosam corporis magni animi cumque soluta quis! Provident cum quia voluptate ab. Cum fugiat eaque possimus consectetur maiores pariatur earum.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis iste ad numquam rerum distinctio nesciunt fuga in? Numquam error quia aspernatur est totam tenetur laboriosam accusamus. Odio non consequatur dicta!Voluptatum error voluptatibus amet esse qui autem, iure eligendi alias laboriosam corporis magni animi cumque soluta quis! Provident cum quia voluptate ab. Cum fugiat eaque possimus consectetur maiores pariatur earum.</p>
+        <p>My name is Wendy Dherin, and I&rsquo;m a full stack developer with a passion for making beautiful audio and visual artworks and building physics engines with <a href="https://p5js.org/" target="_blank" rel="noreferrer">p5.js</a>.</p>
+        <p>In 2014 I built <a href="https://www.audiosnowflake.com" target="_blank" rel="noreferrer">Audio Snowflake</a>, a music-visualizer that creates unique spirographs for songs based on their musical properties, using a predecessor of p5.js, <a href="https://www.processing.org/" target="_blank" rel="noreferrer">Processing</a>.</p>
+        <p>Update on Audio Snowflake: It&rsquo;s currently not fully functional because the Echonest API it relies on has gone the way of the dodo; only songs that are already in the database are searchable. So you can try to search and you might get lucky and get a snowflake, otherwise, search just on the artists, and you might get a different song by them. I hope to soon update the app to use the Spotify API, which has replaced the Echonest one.</p>
+        <p>I&rsquo;m originally from California, but I currently reside in Ireland.</p>
       </div>
     </Layout>
   )
